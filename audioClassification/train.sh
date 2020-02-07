13f27cd0100241c85cc660960fb27581ee87ba93
Jacob Beck <jacobsbeck@hotmail.com>
2020-02-07 00:28:51 -0800

    Added clap classifying files for training

---
+++ audioClassification/train.sh
@@ -0,0 +1,2 @@
+PYTHONPATH='/home/pi/berryconda3/lib/python36.zip','/home/pi/berryconda3/lib/python3.6','/home/pi/berryconda3/lib/python3.6/lib-dynload','/home/pi/.local/lib/python3.6/site-packages','/home/pi/.local/lib/python3.6/site-packages/scipy-1.2.3-py3.6-linux-armv7l.egg','/home/pi/berryconda3/lib/python3.6/site-packages','/home/pi/berryconda3/lib/python3.6/site-packages/scipy-1.2.3-py3.6-linux-armv7l.egg','/home/pi/Prism/rpi_ws281x/python/build/lib.linus-armv7l-2.7'
+/home/pi/berryconda3/bin/python cnn.py -t
