1a5c323597ad094642ab19fb4ba085a1732732a6
Jacob Beck <jacobsbeck@hotmail.com>
2020-05-21 01:57:45 -0700

    Testing audio quality

---
+++ exampleMicrophoneTest.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import wave
+
+r = sr.Recognizer()
+
+sampleRate = 44100.0 
+while True:
+        mic = sr.Microphone()
+        try:
+            with mic as source:
+                r.adjust_for_ambient_noise(source)
+                print("recording..")
+                audio = r.record(source, 3)
+                o = wave.open('demo0.wav', 'w')
+                o.setnchannels(1)
+                o.setsampwidth(2)
+                o.setframerate(sampleRate)
+                o.writeframesraw(audio.get_raw_data())
+                o.close()
+                break
+            
+        except KeyboardInterrupt:
+            break
